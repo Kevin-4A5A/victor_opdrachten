9f482fb6467ff542baff69ca0ed7d4c436f41704
Kevin-4A5A <488661@edu.rocmn.nl>
2017-09-21 11:41:52 +0200

    bmi fixed

--- J3P1/BMI.php
+++ J3P1/BMI.php
@@ -2,34 +2,13 @@
 	session_start();
 	$GLOBALS['BMI'] = 0;
 	
-	function achtergrond()
-	{
-		if($GLOBALS['BMI'] < 18.5)
-		{	
-			$_SESSION['Achtergrond'] = "yellow";
-		}
-		else if($GLOBALS['BMI'] < 25)
-		{
-			$_SESSION['Achtergrond'] = "green";
-		}
-		else if($GLOBALS['BMI'] < 30)
-		{
-			$_SESSION['Achtergrond'] = "red";
-		}
-		else
-		{
-			$_SESSION['Achtergrond'] = "purple";
-		}		
-	}
-	
 	function berekenBMI()
 	{
 		if((isset($_POST['submit'])) && (isset($_POST['lang'])) && (isset($_POST['dikte']))  )
 		{
-			$$GLOBALS['BMI'] = $_POST['dikte'] / ($_POST['lang'] * $_POST['lang']);
-				
-		achtergrond();
-		return $$GLOBALS['BMI'];	
+			$GLOBALS['BMI'] = $_POST['dikte'] / ($_POST['lang'] * $_POST['lang']);
+
+			return $GLOBALS['BMI'];	
 		}
 	}
 ?>
@@ -38,20 +17,6 @@
 <head>
 <title></title>
 <style>
-body
-{
-	background-color: 
-	<?php 
-	if(isset($GLOBALS['BMI']))
-	{
-		echo $GLOBALS['BMI'];
-	}
-	else
-	{ 
-		echo $_SESSION['Achtergrond'];
-	}   
-	?> 
-}
 </style>
 </head>
 <body>
